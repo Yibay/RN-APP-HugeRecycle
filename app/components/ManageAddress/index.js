eab930b7b92a27801096eba5d699454844bce09b
Yibay <514531536@qq.com>
2017-08-09 15:46:04 +0800

    地址管理页 展示地址列表

--- app/components/ManageAddress/index.js
+++ app/components/ManageAddress/index.js
@@ -24,9 +24,9 @@ class ManageAddress extends Component {
 		return (
 			<View style={styles.container}>
 				<Header navigation={this.props.navigation} title='地址管理' />
-				<ListView 
+				<ListView style={styles.addressList}
 					dataSource={this.state.dataSource}
-					renderRow={this._renderRow}
+					renderRow={this._renderRow.bind(this)}
 					enableEmptySections={true} />
 			</View>
 		)
@@ -53,14 +53,88 @@ class ManageAddress extends Component {
 	_renderRow(row) {
 		console.log(row);
 		return (
-			<Text>{row.street}</Text>
+			<View style={styles.addressItem}>
+				<View style={styles.nameTel}>
+					<Text style={styles.name}>{row.customerName}</Text>
+					<Text style={styles.tel}>{row.telNo}</Text>
+				</View>
+				<Text style={styles.address}>
+					{ this._joinAddress(row) }
+				</Text>
+				<View style={styles.operation}>
+					<View style={[styles.operationItem, styles.operationFirstItem]}>
+						<Text style={styles.operationItemTxt}>默认</Text>
+					</View>
+					<View style={styles.operationItem}>
+						<Text style={styles.operationItemTxt}>编辑</Text>
+					</View>
+					<View style={styles.operationItem}>
+						<Text style={styles.operationItemTxt}>删除</Text>
+					</View>
+				</View>
+			</View>
 		)
 	}
+
+	_joinAddress(row){
+		let address = row.city + row.region + row.street + row.communityName;
+		if(row.haveHouseNumber){
+			address += row.building + '幢' + row.unit + '单元' + row.room + '室';
+		}
+		else {
+			address += row.address;
+		}
+		return address;
+	}
+
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1
+	},
+	addressList: {
+		flex: 1
+	},
+	addressItem: {
+		marginTop: 12,
+		backgroundColor: '#fff'
+	},
+	nameTel: {
+		paddingHorizontal: 20,
+		flexDirection: 'row',
+		justifyContent: 'space-between'
+	},
+	name: {
+		fontSize: 16,
+		lineHeight: 35
+	},
+	tel: {
+		fontSize: 13,
+		lineHeight: 35
+	},
+	address: {
+		paddingLeft: 20,
+		lineHeight: 35,
+		color: '#666'
+	},
+	operation: {
+		flexDirection: 'row',
+		justifyContent: 'space-between'
+	},
+	operationItem: {
+		flex: 1,
+		borderTopWidth: 1,
+		borderLeftWidth: 1,
+		borderColor: '#e5e5e5'
+	},
+	operationFirstItem: {
+		borderLeftWidth: 0
+	},
+	operationItemTxt: {
+		lineHeight: 35,
+		textAlign: 'center',
+		color: '#666'
 	}
 })
 
