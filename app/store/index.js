bd668070d18e6bd474398e004d673f3d765ca5ef
Yibay <514531536@qq.com>
2017-08-18 18:59:11 +0800

    createStore时，将本地存储的身份数据初始化store的state

--- app/store/index.js
+++ app/store/index.js
@@ -2,14 +2,37 @@
  *  生成 store对象
  */
 
+// 获取 本地存储 模块
+import { AsyncStorage } from 'react-native';
 // 引入 生成 store的函数
 import { createStore } from 'redux';
 
 // 引入 合并后的 reducer 函数
 import appReducers from '../reducers/index';
 
-// 生成 store
-let store = createStore(appReducers);
+let user_info = {
+	'X-AUTH-TOKEN': '',
+	h5Code: '',
+	user: {
+		id: null,
+		name: '',
+		phone: ''
+	}
+}
+
+AsyncStorage.getItem('X-AUTH-TOKEN')
+	.then(token => user_info['X-AUTH-TOKEN'] = token ? token : '')
+AsyncStorage.getItem('h5Code')
+	.then(h5Code => user_info['h5Code'] = h5Code ? h5Code : '')
+AsyncStorage.getItem('user_id')
+	.then(user_id => user_info.user.id = user_id ? user_id : null)
+AsyncStorage.getItem('user_name')
+	.then(user_name => user_info.user.name = user_name ? user_name : '')
+AsyncStorage.getItem('user_phone')
+	.then(user_phone => user_info.user.phone = user_phone ? user_phone : '')
+
+// 生成 store (第1参数为 reducer函数；第2参数为 初始化state)
+let store = createStore(appReducers, {userInfo: user_info});
 // 首次打印 store的state
 console.log(store.getState());
 
